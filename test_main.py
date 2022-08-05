438d97e8da5545d167457ab13ffcfa8524199af2
princewhyte dabotubo <princewhyte02@gmail.com>
2022-08-05 12:53:40 +0100

    test failing disrupting build

--- test_main.py
+++ test_main.py
@@ -26,6 +26,8 @@ def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
     assert response.json == 'Healthy'
+    #assert false
+    assert False
 
 
 def test_auth(client):
